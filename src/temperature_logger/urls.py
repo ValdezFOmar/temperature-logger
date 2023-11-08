fa4012ed30d86e12f09de6a42767f8904a3e0af1
Omar Valdez <80359343+ValdezFOmar@users.noreply.github.com>
2023-11-08 02:24:22 -0800

    Basic webpage integration

--- src/temperature_logger/urls.py
+++ src/temperature_logger/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage, name="Home"),
+    path("", views.homepage, name="home"),
+    path("temperatures/<date>/", views.temperatures_chart, name="temperatures"),
 ]
--- src/temperature_logger/views.py
+++ src/temperature_logger/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+
+from .models import DateLog, TemperatureReading
 
 
 def homepage(request):
-    template = loader.get_template("home.html")
-    return HttpResponse(template.render())
+    most_recent_date = DateLog.objects.order_by("-date")[0]
+    date = most_recent_date.date.isoformat()
+    context = {
+        "date": date,
+    }
+    return render(request, "home.html", context)
+
+
+def temperatures_chart(request, date):
+    date_log = DateLog.objects.get(date=date)
+    dates = DateLog.objects.exclude(date=date).order_by("-date")
+    readings = TemperatureReading.objects.filter(date=date_log.id).order_by("-time")  # type: ignore
+
+    context = {
+        "selected_date": date_log,
+        "date_logs": dates,
+        "readings": readings,
+    }
+    return render(request, "temperatures.html", context)
